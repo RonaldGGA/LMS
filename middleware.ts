5ac870f06bdea0fe70006a4038c711ce45a9c940
Ronald <ronald.dearmass@gmail.com>
2025-02-22 13:16:32 -0500

    desactivating middleware

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,39 @@
 import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
-import { Role } from "@prisma/client";
+// import { getToken } from "next-auth/jwt";
+// import { Role } from "@prisma/client";
 
 import { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const { nextUrl } = req;
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-  const isLoggedIn = !!token;
+  // const { nextUrl } = req;
+  // const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  // const isLoggedIn = !!token;
 
-  const authRoutes = ["/auth/login", "/auth/register"];
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isApiRoute = nextUrl.pathname.startsWith("/api");
+  // const authRoutes = ["/auth/login", "/auth/register"];
+  // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  // const isApiRoute = nextUrl.pathname.startsWith("/api");
 
-  if (isApiRoute) {
-    return NextResponse.next();
-  }
+  // if (isApiRoute) {
+  //   return NextResponse.next();
+  // }
 
-  // If not logged in
-  if (!isLoggedIn && !isAuthRoute) {
-    return NextResponse.redirect(new URL("/auth/login", nextUrl));
-  }
+  // // If not logged in
+  // if (!isLoggedIn && !isAuthRoute) {
+  //   return NextResponse.redirect(new URL("/auth/login", nextUrl));
+  // }
 
-  // If logged in
-  if (isLoggedIn && isAuthRoute) {
-    const user = token;
-    if (
-      user &&
-      (user.role === Role.LIBRARIAN || user.role === Role.SUPERADMIN)
-    ) {
-      return NextResponse.redirect(new URL("/dashboard", nextUrl));
-    }
-    return NextResponse.redirect(new URL("/", nextUrl));
-  }
+  // // If logged in
+  // if (isLoggedIn && isAuthRoute) {
+  //   const user = token;
+  //   if (
+  //     user &&
+  //     (user.role === Role.LIBRARIAN || user.role === Role.SUPERADMIN)
+  //   ) {
+  //     return NextResponse.redirect(new URL("/dashboard", nextUrl));
+  //   }
+  //   return NextResponse.redirect(new URL("/", nextUrl));
+  // }
+  return NextResponse.next();
 }
 export const config = {
   matcher: [
