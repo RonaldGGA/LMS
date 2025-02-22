998e172f691a5c81c2ec0aa3b0d437f0da3cc6c7
Ronald <ronald.dearmass@gmail.com>
2025-02-22 13:22:10 -0500

    middlware all next

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,25 @@
 import { NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
+import { getToken } from "next-auth/jwt";
 // import { Role } from "@prisma/client";
 
 import { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  // const { nextUrl } = req;
-  // const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-  // const isLoggedIn = !!token;
+  const { nextUrl } = req;
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const isLoggedIn = !!token;
 
   // const authRoutes = ["/auth/login", "/auth/register"];
   // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  // const isApiRoute = nextUrl.pathname.startsWith("/api");
+  const isApiRoute = nextUrl.pathname.startsWith("/api");
 
-  // if (isApiRoute) {
-  //   return NextResponse.next();
-  // }
+  if (isApiRoute) {
+    return NextResponse.next();
+  }
+
+  if (isLoggedIn) {
+    return NextResponse.next();
+  }
 
   // // If not logged in
   // if (!isLoggedIn && !isAuthRoute) {
