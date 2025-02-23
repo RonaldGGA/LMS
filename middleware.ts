c806ae4026e4f7695920fe3b6b7a7f4cd7343640
Ronald <ronald.dearmass@gmail.com>
2025-02-22 19:39:57 -0500

    cloudinary fixed, for now

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-// import { Role } from "@prisma/client";
+import { Role } from "@prisma/client";
 
 import { NextRequest } from "next/server";
 
@@ -9,46 +9,42 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const isLoggedIn = !!token;
-  // const isAdminRoute = nextUrl.pathname.startsWith("/dashboard");
-  // const isAuthRoute = ["/auth/login", "/auth/register"].includes(
-  //   nextUrl.pathname
-  // );
+  const isAdminRoute = nextUrl.pathname.startsWith("/dashboard");
+  const isAuthRoute = ["/auth/login", "/auth/register"].includes(
+    nextUrl.pathname
+  );
   const isApiRoute = nextUrl.pathname.startsWith("/api");
 
   if (isApiRoute) {
     return NextResponse.next();
   }
 
-  if (isLoggedIn) {
-    return NextResponse.next();
+  // If not logged in
+  if (!isLoggedIn && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
   }
 
-  // // If not logged in
-  // if (!isLoggedIn && !isAuthRoute) {
-  //   return NextResponse.redirect(new URL("/auth/login", nextUrl));
-  // }
-
-  // // If logged in
-  // if (isLoggedIn) {
-  //   // handle auth routes
-  //   if (isAuthRoute) {
-  //     if (
-  //       token &&
-  //       (token.role === Role.LIBRARIAN || token.role === Role.SUPERADMIN)
-  //     ) {
-  //       return NextResponse.redirect(new URL("/dashboard", nextUrl));
-  //     }
-  //     return NextResponse.redirect(new URL("/", nextUrl));
-  //   }
-  //   // handle admin routes
-  //   if (isAdminRoute) {
-  //     if (token && token.role === Role.MEMBER) {
-  //       return NextResponse.redirect(new URL("/", nextUrl));
-  //     }
-  //     return NextResponse.next();
-  //   }
-  //   return NextResponse.next();
-  // }
+  // If logged in
+  if (isLoggedIn) {
+    // handle auth routes
+    if (isAuthRoute) {
+      if (
+        token &&
+        (token.role === Role.LIBRARIAN || token.role === Role.SUPERADMIN)
+      ) {
+        return NextResponse.redirect(new URL("/dashboard", nextUrl));
+      }
+      return NextResponse.redirect(new URL("/", nextUrl));
+    }
+    // handle admin routes
+    if (isAdminRoute) {
+      if (token && token.role === Role.MEMBER) {
+        return NextResponse.redirect(new URL("/", nextUrl));
+      }
+      return NextResponse.next();
+    }
+    return NextResponse.next();
+  }
   return NextResponse.next();
 }
 
